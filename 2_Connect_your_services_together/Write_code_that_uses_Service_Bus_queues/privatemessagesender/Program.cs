23ca980a5fb0b6fa97167f5bc6241d7f6edbabd0
Abdelrahmanyassin786 <63580515+Abdelrahmanyassin786@users.noreply.github.com>
2021-12-01 19:28:56 +0200

    Create Program.cs

---
+++ 2_Connect_your_services_together/Write_code_that_uses_Service_Bus_queues/privatemessagesender/Program.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+
+namespace privatemessagesender
+{
+    class Program
+    {
+        const string ServiceBusConnectionString = "Endpoint=sb://<REMOVED>.servicebus.windows.net/;SharedAccessKeyName=<REMOVED>;SharedAccessKey=<REMOVED>";
+        const string QueueName = "salesmessages";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sending a message to the Sales Messages queue...");
+            SendSalesMessageAsync().GetAwaiter().GetResult();
+            Console.WriteLine("Message was sent successfully.");
+        }
+
+        static async Task SendSalesMessageAsync()
+        {
+            await using var client = new ServiceBusClient(ServiceBusConnectionString);
+
+            await using ServiceBusSender sender = client.CreateSender(QueueName);
+            try
+            {
+                string messageBody = $"$10,000 order for bicycle parts from retailer Adventure Works.";
+                var message = new ServiceBusMessage(messageBody);
+                Console.WriteLine($"Sending message: {messageBody}");
+                await sender.SendMessageAsync(message);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"{DateTime.Now} :: Exception: {exception.Message}");
+            }
+        //await queueClient.CloseAsync();    
+        }
+    }
+}
