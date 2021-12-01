d39dd04ae3b0561465c672579378249d703e3fd2
Abdelrahmanyassin786 <63580515+Abdelrahmanyassin786@users.noreply.github.com>
2021-12-01 19:48:40 +0200

    Create Program.cs

---
+++ 2_Connect_your_services_together/Write_code_that_uses_Service_Bus_queues/privatemessagereceiver/Program.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+
+namespace privatemessagereceiver
+{
+    class Program
+    {
+
+        const string ServiceBusConnectionString = "Endpoint=sb://<REMOVED>.servicebus.windows.net/;SharedAccessKeyName=<REMOVED>;SharedAccessKey=<REMOVED>";
+        const string QueueName = "salesmessages";
+
+        static void Main(string[] args)
+        {
+
+            ReceiveSalesMessageAsync().GetAwaiter().GetResult();
+
+        }
+
+        static async Task ReceiveSalesMessageAsync()
+        {
+            Console.WriteLine("======================================================");
+            Console.WriteLine("Press ENTER on the keyboard to exit after receiving all the messages.");
+            Console.WriteLine("======================================================");
+
+            var client = new ServiceBusClient(ServiceBusConnectionString);
+
+            var processorOptions = new ServiceBusProcessorOptions
+            {
+                MaxConcurrentCalls = 1,
+                AutoCompleteMessages = false
+            };
+
+            ServiceBusProcessor processor = client.CreateProcessor(QueueName, processorOptions);
+
+            //await processor.StartProcessingAsync();
+
+            Console.Read();
+
+            // Since we didn't use the "await using" syntax here, we need to explicitly dispose the processor and client
+            await processor.DisposeAsync();
+            await client.DisposeAsync();
+        }
+
+        static async Task MessageHandler(ProcessMessageEventArgs args)
+        {
+            Console.WriteLine($"Received message: SequenceNumber:{args.Message.SequenceNumber} Body:{args.Message.Body}");
+            await args.CompleteMessageAsync(args.Message);
+        }
+
+        static Task ErrorHandler(ProcessErrorEventArgs args)
+        {
+            Console.WriteLine($"Message handler encountered an exception {args.Exception}.");
+            Console.WriteLine("Exception context for troubleshooting:");
+            Console.WriteLine($"- Endpoint: {args.FullyQualifiedNamespace}");
+            Console.WriteLine($"- Entity Path: {args.EntityPath}");
+            Console.WriteLine($"- Executing Action: {args.ErrorSource}");
+            return Task.CompletedTask;
+        }   
+    }
+}
